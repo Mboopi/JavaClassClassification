6955cd91a825ea74d72a27918a938ea58cd38a56
Mboopi <38960170+Mboopi@users.noreply.github.com>
2023-03-20 16:23:02 +0100

    Fix bug

--- test2.py
+++ test2.py
@@ -1,15 +1,20 @@
 import xml.etree.ElementTree as ET
 import re
 
-file_path = "./feature_extraction/raw_data/test_project/CallTree.xml"
+file_path = "./feature_extraction/execution_info/test_project/Applet/Applet_CallTree.xml"
 tree = ET.parse(file_path)
 root = tree.getroot()
 
-file_path2 = "./feature_extraction/raw_data/test_project/CallTree2.xml"
+file_path2 = "./feature_extraction/execution_info/test_project/Bootstrap/Bootstrap_CallTree.xml"
 tree2 = ET.parse(file_path2)
 root2 = tree2.getroot()
 
+file_path3 = "./feature_extraction/execution_info/test_project/Viewer/Viewer_CallTree.xml"
+tree3 = ET.parse(file_path3)
+root3 = tree3.getroot()
+
 root.extend(root2)
+root.extend(root3)
 
 for child in root.iter():
     if child.tag == "node":
@@ -23,7 +28,7 @@ for child in root.iter():
             child.set("class", re.sub(regex, "", class_name))
         # print(child.tag, child.attrib)
 
-with open("./feature_extraction/raw_data/test_project/CallTreePROCESSED.xml", "wb") as f:
+with open("./feature_extraction/CallTreePROCESSED.xml", "wb") as f:
     tree.write(f, encoding="utf-8")
   
 # test = root.findall(".//node[@class='$']")
