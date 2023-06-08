cb0d43bafedb76b8793035dee4419f9cbfe2da16
Mboopi <38960170+Mboopi@users.noreply.github.com>
2023-06-08 11:44:39 +0200

    Update relabeler to consider DISCARD labels

--- feature_extraction/relabeler.py
+++ feature_extraction/relabeler.py
@@ -8,16 +8,16 @@ def apply_new_label(x):
         return x["label"]
 
 original_CSV = pd.read_csv("/Users/mboopi/Documents/GitHub/JavaClassClassification/data/dataset/sweethome3d/features_sweethome3d_FINAL_v4.csv")
-relabeled = pd.read_csv("/Users/mboopi/Desktop/sweethome3d_relabeled.csv", delimiter=";").drop(columns="explanation")
+relabeled = pd.read_csv("/Users/mboopi/Desktop/sweethome3d_relabeled_utf.csv", delimiter=";").drop(columns=["remark", "responsibility"])
 
 labels_dict = {"CO": "Coordinator", "CT": "Controller", "ST": "Structurer",
-               "IT": "Interfacer", "IH": "Information Holder", "SP": "Service Provider"}
+               "IT": "Interfacer", "IH": "Information Holder", "SP": "Service Provider", "DISCARD": "DISCARD"}
 
 # Do left join to keep the original rows, then if new label for row exist,
 # apply that label otherwise keep original label.
 new_CSV = pd.merge(original_CSV, relabeled, on="className", how="left")
 # new_CSV["label_temp"] = new_CSV.apply(lambda x: x["labelNew"] if x["labelNew"] else x["label"], axis=1)
 new_CSV["label"] = new_CSV.apply(lambda x: apply_new_label(x), axis=1)
-new_CSV = new_CSV.drop(columns=["labelOld", "labelNew"])
+new_CSV = new_CSV.drop(columns=["labelOld", "labelNew", "labelSecondary"])
 
-new_CSV.to_csv("/Users/mboopi/Documents/GitHub/JavaClassClassification/data/dataset/sweethome3d/features_sweethome3d_FINAL_v4_RELABELED.csv", index=False)
+new_CSV.to_csv("/Users/mboopi/Documents/GitHub/JavaClassClassification/data/dataset/sweethome3d/features_sweethome3d_FINAL_v4_RELABELED3.csv", index=False)
