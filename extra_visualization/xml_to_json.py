6da02643001aacde92103eb9ddf015d5bc490c82
Mboopi <38960170+Mboopi@users.noreply.github.com>
2023-06-08 11:45:28 +0200

    Replace hardcoded values in XML to JSON converter

--- extra_visualization/xml_to_json.py
+++ extra_visualization/xml_to_json.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 import json
 import hashlib
 import re
+import os
 
 def check_name(node):
     regex = "[$]\d+"
@@ -9,13 +10,12 @@ def check_name(node):
     if bool(re.search(regex, class_name)):
         node.set("class", re.sub(regex, "", class_name))
 
-def traverse_call_tree(parent_node, children, firstCall=False):
+def traverse_call_tree(parent_node, children, whitelist_package, firstCall=False):
     if not firstCall:
         check_name(parent_node)
         parent_name = parent_node.attrib["class"]
 
-        if "sweethome3d" in parent_name:
-        # if "CH.ifa" in parent_name:
+        if whitelist_package in parent_name:
             nodes.add(parent_name)
 
     for child in children:
@@ -24,8 +24,7 @@ def traverse_call_tree(parent_node, children, firstCall=False):
             child_name = child.attrib["class"]
             child_method = child.attrib["methodName"]
 
-            if "sweethome3d" in child_name:
-            # if "CH.ifa" in child_name:
+            if whitelist_package in child_name:
                 nodes.add(child_name)
 
                 if "init" in child_method or "cinit" in child_method:
@@ -33,11 +32,10 @@ def traverse_call_tree(parent_node, children, firstCall=False):
                 else:
                     edge = (parent_name, child_name, "calls")
                 
-                if "sweethome3d" in parent_name and not parent_name == child_name:
-                # if "CH.ifa" in parent_name and not parent_name == child_name:
+                if whitelist_package in parent_name and not parent_name == child_name:
                     edges.add(edge)
 
-        traverse_call_tree(child, child.findall("node"))
+        traverse_call_tree(child, child.findall("node"), whitelist_package)
 
 def node_sub_json(node, current_trace):
     output = {
@@ -93,8 +91,8 @@ def construct_json(nodes, edges, current_trace):
 
 
 if __name__ == "__main__":
-    PROJECT_NAME = "sweethome3d_part2"
-    # PROJECT_NAME = "JHotDraw"
+    # PROJECT_NAME = "sweethome3d_part2"
+    PROJECT_NAME = "jhotdraw"
 
     base_path = f"./feature_extraction/execution_info/{PROJECT_NAME}/"
     # folder_names = ["ActionBar", "FurnitureCatalog", "GeneralScenario", "HomeFurnitureList", "HomePlan", "HomeView",
@@ -102,8 +100,11 @@ if __name__ == "__main__":
     # folder_names = ["ConnectionTool", "ElbowConnectionTool", "EllipseTool", "Initialization", "LineTool", "MenuAlign",
     #                 "MenuAttributes", "MenuDebug", "MenuEdit", "MenuFile", "PolygonTool", "RectangleTool", "RoundRectangleTool",
     #                 "SelectionTool", "TextTool"]
-    folder_names = ["3DS", "Applet", "Bootstrap", "Exception", "HomePlan", "Photo", "TestCases", "Viewer"]
-    
+    # folder_names = ["3DS", "Applet", "Bootstrap", "Exception", "HomePlan", "Photo", "TestCases", "Viewer"]
+    folder_names = os.listdir(base_path)
+    if ".DS_Store" in folder_names:
+        folder_names.remove(".DS_Store")
+
     for folder in folder_names:
         file_path = folder + f"/{folder}_CallTree.xml"
 
@@ -112,9 +113,9 @@ if __name__ == "__main__":
         nodes = set() # Set of nodes to be constructed.
         edges = set() # Set of edges to be constructed (source, target, calls/creates).
 
-        traverse_call_tree(call_tree, call_tree.findall("node"), firstCall=True)
+        traverse_call_tree(call_tree, call_tree.findall("node"), whitelist_package="CH.ifa", firstCall=True)
         final_output = construct_json(nodes, edges, current_trace=folder)
 
-        with open(f"/Users/mboopi/Documents/GitHub/JavaClassClassification/extra_visualization/{PROJECT_NAME}_{folder}.json", "w") as outfile:
+        with open(f"/Users/mboopi/Documents/GitHub/JavaClassClassification/extra_visualization/{PROJECT_NAME}/{PROJECT_NAME}_{folder}.json", "w") as outfile:
             json.dump(final_output, outfile, indent=2)
         outfile.close()
